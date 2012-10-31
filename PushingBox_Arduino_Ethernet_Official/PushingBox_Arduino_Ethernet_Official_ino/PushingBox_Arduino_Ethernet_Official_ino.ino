8bcedc6b93ad7169d11bb14b831fe2e7ddec830a
Clement Storck <clement@pushingbox.com>
2012-10-31 16:21:48 +0100

    Adding more debugging help

---
+++ PushingBox_Arduino_Ethernet_Official/PushingBox_Arduino_Ethernet_Official_ino/PushingBox_Arduino_Ethernet_Official_ino.ino
@@ -0,0 +1,117 @@
+////
+//
+// General code from http://www.pushingbox.com for Arduino + Ethernet Shield (official)
+//
+////
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+  /////////////////
+ // MODIFY HERE //
+/////////////////
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x19 };   // Be sure this address is unique in your network
+
+//Your secret DevID from PushingBox.com. You can use multiple DevID  on multiple Pin if you want
+#define DEVID1 "Your_DevID_Here"        //Scenario : "The mailbox is open"
+
+//Numeric Pin where you connect your switch
+#define pinDevid1 3  // Example : the mailbox switch is connect to the Pin 3
+
+// Debug mode
+#define DEBUG true
+  //////////////
+ //   End    //
+//////////////
+
+
+char serverName[] = "api.pushingbox.com";
+boolean pinDevid1State = false;
+boolean lastConnected = false;                 // state of the connection last time through the main loop
+
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server 
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinDevid1, INPUT);
+  
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    while(true);
+  }
+  else{
+    Serial.println("Ethernet ready");
+    // print the Ethernet board/shield's IP address:
+    Serial.print("My IP address: ");
+    Serial.println(Ethernet.localIP());
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+}
+
+void loop()
+{
+      ////
+      // Listening for the pinDevid1 state
+      ////
+      if (digitalRead(pinDevid1) == HIGH && pinDevid1State == false) // switch on pinDevid1 is ON 
+      {
+        if(DEBUG){Serial.println("pinDevid1 is HIGH");}
+        pinDevid1State = true;
+        //Sending request to PushingBox when the pin is HIGHT
+        sendToPushingBox(DEVID1);
+      }
+       if (digitalRead(pinDevid1) == LOW && pinDevid1State == true) // switch on pinDevid1 is OFF
+      {
+        if(DEBUG){Serial.println("pinDevid1 is LOW");}
+        pinDevid1State = false;
+        //Sending request to PushingBox when the pin is LOW
+        //sendToPushingBox(DEVID1);    //Here you can run an other scenario by creating a DEVID2 variable
+      }
+      
+      
+      //DEBUG part
+      // this write the respons from PushingBox Server.
+      // You should see a "200 OK"
+      if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+      }
+      
+      // if there's no net connection, but there was one last time
+      // through the loop, then stop the client:
+      if (!client.connected() && lastConnected) {
+        Serial.println();
+        Serial.println("disconnecting.");
+        client.stop();
+      }
+      lastConnected = client.connected();
+}
+
+
+//Function for sending the request to PushingBox
+void sendToPushingBox(String devid){
+    if(DEBUG){Serial.println("connecting...");}
+
+  if (client.connect(serverName, 80)) {
+    if(DEBUG){Serial.println("connected");}
+
+    if(DEBUG){Serial.println("sendind request");}
+    client.print("GET /pushingbox?devid=");
+    client.print(devid);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(serverName);
+    client.println("User-Agent: Arduino");
+    client.println();
+  } 
+  else {
+    if(DEBUG){Serial.println("connection failed");}
+  }
+}
